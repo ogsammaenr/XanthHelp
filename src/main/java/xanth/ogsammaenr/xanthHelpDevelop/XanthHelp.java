ed50e6674caae00c087ae99ed93affd628971f42
ogsammaenr <osmangamer152@gmail.com>
2025-06-30 23:22:23 +0300

    Initial Commit

---
+++ src/main/java/xanth/ogsammaenr/xanthHelpDevelop/XanthHelp.java
@@ -0,0 +1,17 @@
+package xanth.ogsammaenr.xanthHelpDevelop;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class XanthHelp extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
