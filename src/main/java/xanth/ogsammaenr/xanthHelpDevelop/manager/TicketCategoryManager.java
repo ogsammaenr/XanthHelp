acf7bd713ac5cc87fc3616857cc0bcae53ba255d
ogsammaenr <osmangamer152@gmail.com>
2025-07-19 12:39:40 +0300

    fix category manager class

--- src/main/java/xanth/ogsammaenr/xanthHelpDevelop/manager/TicketCategoryManager.java
+++ src/main/java/xanth/ogsammaenr/xanthHelpDevelop/manager/TicketCategoryManager.java
@@ -4,8 +4,8 @@ import xanth.ogsammaenr.xanthHelpDevelop.XanthHelp;
 import xanth.ogsammaenr.xanthHelpDevelop.model.TicketCategory;
 import xanth.ogsammaenr.xanthHelpDevelop.model.TicketCategoryType;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class TicketCategoryManager {
     private final XanthHelp plugin;
@@ -19,19 +19,39 @@ public class TicketCategoryManager {
         types = new HashMap<>();
     }
 
-    public TicketCategory getCategoryById(String categoryid) {
-        return categories.get(categoryid);
+    public TicketCategory getCategoryById(String categoryId) {
+        return categories.get(categoryId);
     }
 
-    public TicketCategoryType getCategoryTypeById(String categoryid) {
-        return types.get(categoryid);
+    public TicketCategoryType getCategoryTypeById(String typeId) {
+        return types.get(typeId);
+    }
+
+    public Collection<TicketCategory> getAllCategories() {
+        return categories.values();
+    }
+
+    public Collection<TicketCategoryType> getAllCategoryTypes() {
+        return types.values();
+    }
+
+    public List<TicketCategory> getCategoriesByType(String typeId) {
+        if (typeId == null) return Collections.emptyList();
+
+        return categories.values().stream()
+                .filter(t -> t.getType() != null && typeId.equals(t.getType().getId()))
+                .collect(Collectors.toList());
     }
 
     public void addCategory(TicketCategory category) {
+        if (category == null || category.getId() == null) return;
+
         categories.put(category.getId(), category);
     }
 
     public void addCategoryType(TicketCategoryType categoryType) {
+        if (categoryType == null || categoryType.getId() == null) return;
+
         types.put(categoryType.getId(), categoryType);
     }
 
